d80aeb6ff676148443f27584a288ed2d8785d3e2
Choi-Big-Root <bigroot0601@gmail.com>
2025-05-27 21:54:07 +0900

    [20250527] ViewModule - DataLayer 로직 구현.

--- lib/data/data_source/mock/display/display_mock_api.dart
+++ lib/data/data_source/mock/display/display_mock_api.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import '../../../dto/common/response_wrapper/response_wrapper.dart';
 import '../../../dto/display/menu/menu.dto.dart';
+import '../../../dto/display/view_module/view_module.dto.dart';
 import '../../remote/display.api.dart';
 import 'display_mock_data.dart';
 
@@ -39,6 +40,53 @@ class DisplayMockApi implements DisplayApi {
             (mallType == 'market')
                 ? _menuParser(DisplayMockData.menusByMarket)
                 : _menuParser(DisplayMockData.menusByBeauty),
+      ),
+    );
+  }
+
+  List<ViewModuleDto> _viewModuleParser(String source) {
+    final dynamic json = jsonDecode(source);
+
+    // json이 List 타입일 때만 처리하고, 각 요소가 Map<String, dynamic>인지 확인
+    if (json is List) {
+      return json
+          .map(
+            (e) =>
+                e is Map<String, dynamic>
+                    ? ViewModuleDto.fromJson(e)
+                    : throw Exception('Invalid data format'),
+          )
+          .toList();
+    } else {
+      // 예외 처리: List가 아닌 경우 빈 리스트 반환
+      return [];
+    }
+  }
+
+  @override
+  Future<ResponseWrapper<List<ViewModuleDto>>> getViewModulesByTabId(
+    int tabId,
+  ) {
+    late String source;
+    final endOfTabId = tabId % 10;
+    switch (endOfTabId) {
+      case 1:
+        source = DisplayMockData.viewModulesByTabIdCaseOne;
+      case 2:
+        source = DisplayMockData.viewModulesByTabIdCaseTwo;
+      case 3:
+        source = DisplayMockData.viewModulesByTabIdCaseThree;
+      case 4:
+        source = DisplayMockData.viewModulesByTabIdCaseFour;
+      case 5:
+        source = DisplayMockData.viewModulesByTabIdCaseFive;
+    }
+    return Future(
+      () => ResponseWrapper(
+        status: 'SUCCESS',
+        code: '0000',
+        message: '',
+        data: _viewModuleParser(source),
       ),
     );
   }
--- lib/data/data_source/mock/display/display_mock_data.dart
+++ lib/data/data_source/mock/display/display_mock_data.dart
@@ -4,4 +4,15 @@ class DisplayMockData {
 
   static const String menusByBeauty =
       '[{"title":"BIG 추천","tabId":20001},{"title":"페스티벌","tabId":20002},{"title":"신상품","tabId":20003},{"title":"베스트","tabId":20004},{"title":"브랜드관","tabId":20005}]';
+
+  static const String viewModulesByTabIdCaseOne =
+      '[{"type":"ViewModuleA"},{"type":"ViewModuleB"},{"type":"ViewModuleC"},{"type":"ViewModuleD"},{"type":"ViewModuleE"}]';
+  static const String viewModulesByTabIdCaseTwo =
+      '[{"type":"ViewModuleB"},{"type":"ViewModuleC"},{"type":"ViewModuleD"},{"type":"ViewModuleE"},{"type":"ViewModuleA"}]';
+  static const String viewModulesByTabIdCaseThree =
+      '[{"type":"ViewModuleC"},{"type":"ViewModuleD"},{"type":"ViewModuleE"},{"type":"ViewModuleA"},{"type":"ViewModuleB"}]';
+  static const String viewModulesByTabIdCaseFour =
+      '[{"type":"ViewModuleD"},{"type":"ViewModuleE"},{"type":"ViewModuleA"},{"type":"ViewModuleB"},{"type":"ViewModuleC"}]';
+  static const String viewModulesByTabIdCaseFive =
+      '[{"type":"ViewModuleE"},{"type":"ViewModuleA"},{"type":"ViewModuleB"},{"type":"ViewModuleC"},{"type":"ViewModuleD"}]';
 }
