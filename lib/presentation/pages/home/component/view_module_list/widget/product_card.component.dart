aef3111cdb88aa9d3611adbabbf94704ccca4beb
Choi-Big-Root <bigroot0601@gmail.com>
2025-07-11 12:15:18 +0900

    [20250711] 상품 카드 컴포넌트 개발
    확장하여 figma 에있는 디자인에 맞게 theme 접근하여 컬러및 스타일 지정. large,small 카드 각 디자인에 맞게 스타일 적용 및 분리

---
+++ lib/presentation/pages/home/component/view_module_list/widget/product_card.component.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'product_card_widget.dart';
+
+class LargeProdictCard extends ProductCardWidget {
+  LargeProdictCard({
+    required this.context,
+    required super.productInfo,
+    super.key,
+  }) : super(
+         imageAspect: 150 / 195,
+         needShowReviewCount: true,
+         titleStyle: Theme.of(context).textTheme.titleSmall,
+         priceStyle: Theme.of(context).textTheme.titleSmall,
+         originalPriceStyle: Theme.of(context).textTheme.labelMedium,
+         reviewStyle: Theme.of(context).textTheme.labelSmall,
+       );
+
+  final BuildContext context;
+}
+
+class SmallProductCard extends ProductCardWidget {
+  SmallProductCard({
+    required this.context,
+    required super.productInfo,
+    super.key,
+  }) : super(
+         imageAspect: 114 / 152,
+         needShowReviewCount: false,
+         titleStyle: Theme.of(context).textTheme.labelMedium,
+         priceStyle: Theme.of(context).textTheme.labelMedium,
+         originalPriceStyle: Theme.of(context).textTheme.labelMedium,
+         reviewStyle: Theme.of(context).textTheme.labelMedium,
+       );
+
+  final BuildContext context;
+}
--- lib/presentation/pages/home/component/view_module_list/widget/product_card_widget.dart
+++ lib/presentation/pages/home/component/view_module_list/widget/product_card_widget.dart
@@ -11,11 +11,21 @@ class ProductCardWidget extends StatelessWidget {
   const ProductCardWidget({
     required this.productInfo,
     required this.imageAspect,
+    required this.needShowReviewCount,
+    this.titleStyle,
+    this.priceStyle,
+    this.originalPriceStyle,
+    this.reviewStyle,
     super.key,
   });
 
   final ProductInfo productInfo;
   final double imageAspect;
+  final TextStyle? titleStyle;
+  final TextStyle? priceStyle;
+  final TextStyle? originalPriceStyle;
+  final TextStyle? reviewStyle;
+  final bool needShowReviewCount;
 
   @override
   Widget build(BuildContext context) {
@@ -39,35 +49,50 @@ class ProductCardWidget extends StatelessWidget {
         //     color: Theme.of(context).colorScheme.contentTertiary,
         //   ),
         // ),
-        Text(productInfo.title, overflow: TextOverflow.ellipsis, maxLines: 2),
+        Text(
+          productInfo.title,
+          style: titleStyle?.titleCopyWith(),
+          overflow: TextOverflow.ellipsis,
+          maxLines: 2,
+        ),
         const SizedBox(height: 1),
         Row(
           children: [
-            Text('${productInfo.discountRate}%'),
+            Text(
+              '${productInfo.discountRate}%',
+              style: priceStyle?.discountRateCopyWith(),
+            ),
             const SizedBox(width: 6),
-            Text(productInfo.price.toWon()),
+            Text(productInfo.price.toWon(), style: priceStyle?.priceCopyWith()),
           ],
         ),
         const SizedBox(height: 3),
         Text(
           productInfo.originalPrice.toWon(),
-          style: const TextStyle(decoration: TextDecoration.lineThrough),
+          style: originalPriceStyle?.originalCopyWith(),
         ),
-        Row(
-          children: [
-            SvgPicture.asset(
-              AppIcons.chat,
-              width: 13,
-              height: 13,
-              colorFilter: ColorFilter.mode(
-                Theme.of(context).colorScheme.contentTertiary,
-                BlendMode.srcIn,
-              ),
+        if (needShowReviewCount)
+          Padding(
+            padding: const EdgeInsets.only(top: 8),
+            child: Row(
+              children: [
+                SvgPicture.asset(
+                  AppIcons.chat,
+                  width: 13,
+                  height: 13,
+                  colorFilter: ColorFilter.mode(
+                    Theme.of(context).colorScheme.contentTertiary,
+                    BlendMode.srcIn,
+                  ),
+                ),
+                const SizedBox(width: 3),
+                Text(
+                  '후기 ${productInfo.reviewCount.toReview()}',
+                  style: reviewStyle?.reviewCountCopyWith(),
+                ),
+              ],
             ),
-            const SizedBox(width: 3),
-            Text('후기 ${productInfo.reviewCount.toReview()}'),
-          ],
-        ),
+          ),
       ],
     );
   }
